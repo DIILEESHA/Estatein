a1a4f84bcc342d6550f1a275eb3c15a920d6a8a7
DIILEESHA <dileeeshanawarathna18@gmail.com>
2024-01-18 18:46:36 +0530

    configure

---
+++ src/pages/home/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import SmNav from "../../components/smnav/SmNav";
+import Nav from "../../components/nav/Nav";
+import Header from "../../components/header/Header";
+import FeatureProperties from "../../components/feautrue-properties/FeatureProperties";
+
+const Home = () => {
+  return (
+    <div>
+      <SmNav />
+      <Nav />
+      <Header />
+      <FeatureProperties />
+    </div>
+  );
+};
+
+export default Home;
+
+
+
+
