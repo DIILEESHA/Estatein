04292fd74772c5fa7ccfd49d54fe80e60850ea67
DIILEESHA <dileeeshanawarathna18@gmail.com>
2024-01-25 00:58:22 +0530

    update fav

--- frontview/src/components/nav/Nav.js
+++ frontview/src/components/nav/Nav.js
@@ -1,37 +1,73 @@
 import React from "react";
 import "./nav.css";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 const Nav = () => {
+  const navigate = useNavigate();
+
+  const scrollToTop = () => {
+    const scrollDuration = 500; // adjust as needed
+    const scrollStep = -window.scrollY / (scrollDuration / 15);
+
+    const scrollInterval = setInterval(() => {
+      if (window.scrollY !== 0) {
+        window.scrollBy(0, scrollStep);
+      } else {
+        clearInterval(scrollInterval);
+      }
+    }, 15);
+  };
+
+  const handleNavLinkClick = (path) => {
+    navigate(path);
+    scrollToTop();
+  };
+
   return (
     <div className="nav_container">
       <div className="nav_grid">
         <div className="nav_sub_grid">
           <div className="nav_logo">
-            <NavLink to="/">
+            <Link to="/" onClick={scrollToTop}>
               <img src="https://i.imgur.com/2Zdmcay.png" alt="" />
-            </NavLink>
+            </Link>
           </div>
         </div>
         <div className="nav_sub_grid">
           <ul className="nav_ul">
             <li className="nav_li">
-              <NavLink className="linka" to="/">
+              <NavLink
+                className="linka"
+                to="/"
+                onClick={() => handleNavLinkClick("/")}
+              >
                 home
               </NavLink>
             </li>
             <li className="nav_li">
-              <NavLink className="linka" to="/about">
+              <NavLink
+                className="linka"
+                to="/about"
+                onClick={() => handleNavLinkClick("/about")}
+              >
                 about us
               </NavLink>
             </li>
             <li className="nav_li">
-              <NavLink className="linka" to="/property">
+              <NavLink
+                className="linka"
+                to="/property"
+                onClick={() => handleNavLinkClick("/property")}
+              >
                 properties
               </NavLink>
             </li>
             <li className="nav_li">
-              <NavLink className="linka" to="/service">
+              <NavLink
+                className="linka"
+                to="/service"
+                onClick={() => handleNavLinkClick("/service")}
+              >
                 services
               </NavLink>
             </li>
@@ -40,7 +76,11 @@ const Nav = () => {
         <div className="nav_sub_grid">
           <ul className="nav_ul">
             <li className="nav_li last">
-              <Link className="linka" to="/contact">
+              <Link
+                className="linka"
+                to="/contact"
+                onClick={() => handleNavLinkClick("/contact")}
+              >
                 contact us
               </Link>
             </li>
